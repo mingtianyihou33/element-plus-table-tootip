01728b10f70947b26120fa389857abb82a5e7dee
mingtianyihou33 <liumingyi1428@126.com>
2021-01-08 18:20:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <p>
+  <router-link to="/">home</router-link> |
+  <router-link to="/about">about</router-link>
+  </p>
+  <router-view></router-view>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+import router from "./router";
+
+createApp(App).use(ElementPlus).use(router).mount('#app')
